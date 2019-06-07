4ceb846a7a75a72741b9822120415b1d9b237918
Joepis <peijie-sh@outlook.com>
2019-06-07 16:21:06 +0800

    delete no-use code

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        String a="fool";
+        
         return "Hello jenkins, " + new Date();
     }
 }
